ab4822b10d89fae348c94e9d73d9c515905b2860
AndHryb <Andryigr@mail.ru>
2019-08-13 23:36:23 +0300

    исправил замечания

--- n.13_drinks_hash/drinks_hash.js
+++ n.13_drinks_hash/drinks_hash.js
@@ -10,16 +10,16 @@ function HashStorage() {
     this.deleteValue= function(key){
         if(key in this.storage){
             delete this.storage[key];
-        }else{
-            return false;
+            return true;
         }
-
+            return false;
     }
     this.getKeys = function(){
         return Object.keys(this.storage);
     }
 }
 var drinkStorage = new HashStorage();
+
 function addDrinkInfo() {
     var drinkName = prompt('Введите название напитка');
     var isAlcogol = confirm('Этот напиток алкогольный?');
@@ -29,13 +29,14 @@ function addDrinkInfo() {
 
 function getDrinkInfo() {
     var askDrinkName = prompt('Введите название напитка,о котором хотите получить информацию');
-    if (askDrinkName in drinkStorage.storage) {
-        console.log((drinkStorage.getValue(askDrinkName).alcogol)? 'Алкогольный напиток':'Безалкогольный Напиток' );
-        console.log('Рецепт: ' + drinkStorage.getValue(askDrinkName).recept);
-    } else {
+    if (drinkStorage.getValue(askDrinkName) === undefined) {
         console.log('Информации об этом напитке не найдено')
+    }else{
+        console.log((drinkStorage.getValue(askDrinkName).alcogol) ? 'Алкогольный напиток' : 'Безалкогольный Напиток');
+        console.log('Рецепт: ' + drinkStorage.getValue(askDrinkName).recept);
     }
 }
+
 function delDrinkInfo () {
     var delDrinkName = prompt('Введите название напитка,информацию о котором хотите удалить');
     if(delDrinkName in drinkStorage.storage){
@@ -44,17 +45,8 @@ function delDrinkInfo () {
         console.log('Информации об этом напитке не найдено')
     }
 }
+
 function getAllDrinks() {
     var allDrinks = drinkStorage.getKeys();
-    function allDrinksInfo(v,i,a) {
-       alert(`
-              напиток ${v}          
-              алкогольный: ${(drinkStorage.storage[v].alcogol)? 'да':'нет'}
-              рецепт приготовления: 
-              ${(drinkStorage.storage[v].recept)} 
-                
-               `)
-
-    }
-    allDrinks.forEach(allDrinksInfo);
+    alert( allDrinks.join(' , '));
 }
